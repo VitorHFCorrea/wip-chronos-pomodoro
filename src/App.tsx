8f3a0a7271338ae2a10c871faad399f5d790a215
VitorHFCorrea <143048290+VitorHFCorrea@users.noreply.github.com>
2025-03-28 18:35:09 -0300

    Rendering adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { FocusSection } from './components/FocusSection'
+import { Footer } from './components/Footer'
+import { Heading } from './components/Heading'
+
 export function App() {
   return (
-    <div className='w-screen h-screen flex justify-center items-center bg-mygray-900'>
-      <h1>Olá, Mundo!</h1>
+    <div className=' flex flex-col w-screen h-screen bg-gradient-to-br from-mygray-900 to-mygray-700 '>
+      <Heading />
+      <FocusSection />
+      <Footer />
     </div>
   )
 }
