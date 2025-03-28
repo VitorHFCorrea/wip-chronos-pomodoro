6a8577a44846db0b11101a312f187b0282c0cf05
VitorHFCorrea <143048290+VitorHFCorrea@users.noreply.github.com>
2025-03-28 02:39:41 -0300

    css files deleted
    index.css is the only needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className='text-'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='w-screen h-screen flex justify-center items-center bg-gray-800'>
+      <h1 className='text-9xl font-semibold text-white'>Olá, Mundo!</h1>
+    </div>
   )
 }
 
