0fc6759f3c488ef15c3ad1df7c424b1b9d5bc68f
VitorHFCorrea <143048290+VitorHFCorrea@users.noreply.github.com>
2025-03-30 21:34:34 -0300

    Light theme created
    index.css w/ new custom colors
    Heading.tsx with new light/dark mode toggler functions
    App.tsx with transition duration for bg color change
    index.html with new data-theme (dark or light)

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { FocusSection } from './components/FocusSection'
 import { Footer } from './components/Footer'
 import { Heading } from './components/Heading'
 
-
 export function App() {
   return (
-    <div className='flex justify-center w-full h-full bg-gradient-to-br from-mygray-900 to-mygray-700'>
+    <div className='flex justify-center w-full h-full bg-gradient-to-br from-mygray-900 to-mygray-700 transition duration-300'>
       <div className='max-w-[98rem] min-h-screen flex flex-col overflow-hidden p-[3.2rem] '>
         <Heading />
         <FocusSection />
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,11 +1,31 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon, TimerIcon } from 'lucide-react'
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon, TimerIcon } from 'lucide-react'
 import { Button } from './Button'
+import { useState, useEffect } from 'react'
 
-// type HeadingProps = {
-//   children: React.ReactNode
-// }
+type AvailableThemes = 'dark'|'light'
 
 export function Heading() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme = localStorage.getItem('theme') as AvailableThemes || 'dark';
+    return storageTheme
+  });
+
+  function handleThemeChange(){
+    setTheme(prevTheme => {     
+    return prevTheme === 'dark' ? 'light' : 'dark'
+    })
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  const nextThemeIcon = {
+    dark: <SunIcon className='transition duration-5000'/>,
+    light: <MoonIcon/>
+  }
+
   return (
     <div className='flex flex-col items-center gap-10'>
       <div className='flex flex-col items-center text-primary cursor-pointer rounded-full'>
@@ -13,10 +33,10 @@ export function Heading() {
         <h1 className='text-[2.70rem] font-bold'>Chronos</h1>
       </div>
       <div className='flex gap-4'>
-        <Button icon = {<HouseIcon/>} />
-        <Button icon = {<HistoryIcon/>}  />
-        <Button icon = {<SettingsIcon/> } />
-        <Button icon = {<SunIcon/>}  />
+        <Button icon = {<HouseIcon/>} title='Home'/>
+        <Button icon = {<HistoryIcon/>} title='Histórico'/>
+        <Button icon = {<SettingsIcon/>} title='Configurações'/>
+        <Button icon = {nextThemeIcon[theme]} title='Tema' onClick={handleThemeChange}/>
       </div>
     </div>
   )
