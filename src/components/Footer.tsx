05254ee0b5d1a4318732962c4684613b07547aee
VitorHFCorrea <143048290+VitorHFCorrea@users.noreply.github.com>
2025-03-30 21:36:08 -0300

    Update Footer.tsx
    margin top edited

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from './Link'
 
 export function Footer() {
   return (
-    <div className='flex flex-col items-center justify-center w-full h-25 -mb-15 gap-1.5'>
+    <div className='flex flex-col items-center justify-center w-full h-25 -mb-15 mt-12 gap-1.5'>
       <Link>Entenda a técnica pomodoro 🍅</Link>
       <Link>Chronos Pomodoro © 2025 - Feito com 💚</Link>
     </div>
